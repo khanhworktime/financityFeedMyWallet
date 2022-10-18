1a79558254c40d191e8e1978a57ff27ecf417885
ChouPham <phamthieuchau@170802@gmail.com>
2022-10-18 19:33:07 +0700

    login + signup

--- app/src/main/java/com/financity/feedmywallet/LogIn.java
+++ app/src/main/java/com/financity/feedmywallet/LogIn.java
@@ -2,13 +2,29 @@ package com.financity.feedmywallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LogIn extends AppCompatActivity {
+
+    TextView to_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        to_signup = findViewById(R.id.tv_signup);
+
+        to_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/financity/feedmywallet/SignUp.java
+++ app/src/main/java/com/financity/feedmywallet/SignUp.java
@@ -2,13 +2,28 @@ package com.financity.feedmywallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
+
+    TextView to_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
+        to_login = findViewById(R.id.tv_login);
+
+        to_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), LogIn.class);
+                startActivity(i);
+            }
+        });
     }
 }
