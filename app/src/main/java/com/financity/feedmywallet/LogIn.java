d202ff06afd308c9985a621976416f83dba4f3c2
khanhworktime <chikhanh5@gmail.com>
2022-10-18 23:10:12 +0700

    Fix notification bar and add Splash screen

--- app/src/main/java/com/financity/feedmywallet/LogIn.java
+++ app/src/main/java/com/financity/feedmywallet/LogIn.java
@@ -30,12 +30,12 @@ public class LogIn extends AppCompatActivity {
 
         btn_login = findViewById(R.id.btn_login);
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), MyWallet.class);
-                startActivity(i);
-            }
-        });
+//        btn_login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i = new Intent(getApplicationContext(), MyWallet.class);
+//                startActivity(i);
+//            }
+//        });
     }
 }
---
+++ app/src/main/java/com/financity/feedmywallet/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.financity.feedmywallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LogIn.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+    }
+}
